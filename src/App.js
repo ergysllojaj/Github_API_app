6fbec0da3ea3e96c11b0c2788d6cd18c6d9ea0b8
Ergys <ergys.llojaj@gmail.com>
2022-07-16 21:15:56 +0200

    installed tailwind and react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
